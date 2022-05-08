50d74848f9c60f64c2aac35dcf667f82cb543575
mxpnt <87723293+mxpnt@users.noreply.github.com>
2022-05-08 19:28:28 +0200

    Add files via upload

---
+++ philo/philo/src/utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 19:03:16 by maxime            #+#    #+#             */
+/*   Updated: 2022/05/03 09:38:47 by mapontil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	is_valid(char *str)
+{
+	if (ft_atolli(str) < 1)
+		return (1);
+	return (0);
+}
+
+int	is_not_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	is_not_limit(char *str)
+{
+	if (ft_atolli(str) < INT_MIN || ft_atolli(str) > INT_MAX)
+		return (1);
+	if (ft_atolli(str) < 0)
+		return (2);
+	return (0);
+}
+
+long long int	ft_atolli(char *str)
+{
+	int				i;
+	long long int	neg;
+	long long int	res;
+
+	i = 0;
+	neg = 1;
+	res = 0;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	return (res * neg);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
